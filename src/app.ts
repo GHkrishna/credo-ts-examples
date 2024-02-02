ec75806e1a7f817520deeb81a0d66fa7854fb8a9
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-02-02 14:30:35 +0530

    refactor: cleanup
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/app.ts
+++ src/app.ts
@@ -20,48 +20,36 @@ async function app() {
   const did = `did:web:${domain}`
   const keyId = `${did}#key-1`
 
-  // const key = await issuer.wallet.createKey({
-  //   keyType: KeyType.Ed25519,
-  //   privateKey: TypedArrayEncoder.fromString('afjdemoverysercure00000000000000'),
-  // })
+  const key = await issuer.wallet.createKey({
+    keyType: KeyType.Ed25519,
+    privateKey: TypedArrayEncoder.fromString('afjdemoverysercure00000000000000'),
+  })
 
-  // const holderKey = await holder.dids.create({
-  //   method: 'key',
-  //   options: {
-  //     keyType: KeyType.Ed25519,
-  //   },
-  //   secret: {
-  //     privateKey: TypedArrayEncoder.fromString('afjdemoverysercure00000000000key'),
-  //   },
-  // })
+  const holderKey = await holder.dids.create({
+    method: 'key',
+    options: {
+      keyType: KeyType.Ed25519,
+    },
+    secret: {
+      privateKey: TypedArrayEncoder.fromString('afjdemoverysercure00000000000key'),
+    },
+  })
 
-  // const didDocument = new DidDocumentBuilder(did)
-  //   .addContext('https://w3id.org/security/suites/ed25519-2018/v1')
-  //   .addVerificationMethod(getEd25519VerificationKey2018({ key, id: keyId, controller: did }))
-  //   .addAuthentication(keyId)
-  //   .build()
+  const didDocument = new DidDocumentBuilder(did)
+    .addContext('https://w3id.org/security/suites/ed25519-2018/v1')
+    .addVerificationMethod(getEd25519VerificationKey2018({ key, id: keyId, controller: did }))
+    .addAuthentication(keyId)
+    .build()
 
-  // await issuer.dids.import({
-  //   did,
-  //   overwrite: true,
-  //   didDocument,
-  // })
-
-  // const issuerId = `did:web:sairanjitaw.github.io`
-  // const privateKey = '73f80dcde8be30e538ea8bafeb4701d098c5ea72720a51dc750527f4b78f01b2'
-  // await issuer.dids.import({
-  //   did: issuerId,
-  //   overwrite: true,
-  //   privateKeys: [
-  //     {
-  //       keyType: KeyType.Ed25519,
-  //       privateKey: TypedArrayEncoder.fromHex(privateKey),
-  //     },
-  //   ],
-  // })
+  await issuer.dids.import({
+    did,
+    overwrite: true,
+    didDocument,
+  })
 
   const dids = await issuer.dids.getCreatedDids({ did })
-  console.log('dids', JSON.stringify(dids))
+
+  const usingDid = dids.find((record) => record.did.includes('did:web'))?.did
 
   // Create out of band invitation
 
@@ -75,62 +63,56 @@ async function app() {
 
   await issuer.connections.returnWhenIsConnected(connectionRecord.id)
 
-  const usingDid = dids.find((record) => record.did.includes('did:web'))?.did
-
-  console.log('\n\n\nusingDid***', usingDid)
-
-  console.log('connectionRecord.did', connectionRecord.did)
-
-  // const credRecord = await issuer.credentials.offerCredential({
-  //   connectionId: connectionRecord.id,
-  //   credentialFormats: {
-  //     jsonld: {
-  //       credential: {
-  //         '@context': ['https://www.w3.org/2018/credentials/v1', 'https://www.w3.org/2018/credentials/examples/v1'],
-  //         type: ['VerifiableCredential', 'UniversityDegreeCredential'],
-  //         issuer: { id: usingDid! },
-  //         issuanceDate: new Date().toISOString(),
-  //         credentialSubject: {
-  //           id: holderKey.didState.did!,
-  //           degree: {
-  //             type: 'BachelorDegree',
-  //             name: 'Bachelor of Science and Arts',
-  //           },
-  //         },
-  //       },
-  //       options: {
-  //         proofType: 'Ed25519Signature2018',
-  //         proofPurpose: 'assertionMethod',
-  //       },
-  //     },
-  //   },
-  //   protocolVersion: 'v2',
-  // })
-
-  const proofRecord = await issuer.proofs.requestProof({
+  const credRecord = await issuer.credentials.offerCredential({
     connectionId: connectionRecord.id,
-    proofFormats: {
-      presentationExchange: {
-        presentationDefinition: {
-          id: '32f54163-7166-48f1-93d8-ff217bdb0653',
-          input_descriptors: [
-            {
-              constraints: {
-                fields: [
-                  {
-                    path: ['$.credentialSubject.degree.type'],
-                  },
-                ],
-              },
-              id: 'citizenship_input_1',
-              schema: [{ uri: 'https://www.w3.org/2018/credentials/examples/v1' }],
+    credentialFormats: {
+      jsonld: {
+        credential: {
+          '@context': ['https://www.w3.org/2018/credentials/v1', 'https://www.w3.org/2018/credentials/examples/v1'],
+          type: ['VerifiableCredential', 'UniversityDegreeCredential'],
+          issuer: { id: usingDid! },
+          issuanceDate: new Date().toISOString(),
+          credentialSubject: {
+            id: holderKey.didState.did!,
+            degree: {
+              type: 'BachelorDegree',
+              name: 'Bachelor of Science and Arts',
             },
-          ],
+          },
+        },
+        options: {
+          proofType: 'Ed25519Signature2018',
+          proofPurpose: 'assertionMethod',
         },
       },
     },
     protocolVersion: 'v2',
   })
+
+  // const proofRecord = await issuer.proofs.requestProof({
+  //   connectionId: connectionRecord.id,
+  //   proofFormats: {
+  //     presentationExchange: {
+  //       presentationDefinition: {
+  //         id: '32f54163-7166-48f1-93d8-ff217bdb0653',
+  //         input_descriptors: [
+  //           {
+  //             constraints: {
+  //               fields: [
+  //                 {
+  //                   path: ['$.credentialSubject.degree.type'],
+  //                 },
+  //               ],
+  //             },
+  //             id: 'citizenship_input_1',
+  //             schema: [{ uri: 'https://www.w3.org/2018/credentials/examples/v1' }],
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   },
+  //   protocolVersion: 'v2',
+  // })
 }
 
 app()
