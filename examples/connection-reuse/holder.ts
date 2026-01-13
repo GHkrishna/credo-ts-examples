9254e9ff5d73807e795dc4264adb799bf6670860
Krishna Waske <krishna.waske@ayanworks.com>
2026-01-13 12:55:25 +0530

    add corsor based pagination and credo v0.6.1 in connection-reuse example
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- examples/connection-reuse/holder.ts
+++ examples/connection-reuse/holder.ts
@@ -1,55 +1,87 @@
 import { AskarModule } from "@credo-ts/askar";
 import {
 	Agent,
-	AutoAcceptCredential,
-	AutoAcceptProof,
-	ConnectionsModule,
-	ConsoleLogger,
-	CredentialsModule,
-	DifPresentationExchangeProofFormatService,
-	HttpOutboundTransport,
-	JsonLdCredentialFormatService,
+		ConsoleLogger,
 	LogLevel,
-	MediationRecipientModule,
-	MediatorPickupStrategy,
-	ProofsModule,
-	V2CredentialProtocol,
-	V2ProofProtocol,
-	WsOutboundTransport,
 } from "@credo-ts/core";
-import { HttpInboundTransport, agentDependencies } from "@credo-ts/node";
-import { ariesAskar } from "@hyperledger/aries-askar-nodejs";
+import {
+	DidCommHttpOutboundTransport,
+	DidCommWsOutboundTransport,
+	DidCommModule
+} from "@credo-ts/didcomm";
+import { DrizzleStorageModule } from "@credo-ts/drizzle-storage";
+import { coreBundle } from "@credo-ts/drizzle-storage/core";
+import { anoncredsBundle } from "@credo-ts/drizzle-storage/anoncreds";
+import { didcommBundle } from "@credo-ts/drizzle-storage/didcomm";
+import { DidCommHttpInboundTransport, agentDependencies } from "@credo-ts/node";
+import { askar } from "@openwallet-foundation/askar-nodejs";
+
+// Postgres
+// import { drizzle } from "drizzle-orm/node-postgres";
+// SQL
+import { drizzle } from "drizzle-orm/libsql";
+import { questionAnswerBundle } from "@credo-ts/drizzle-storage/question-answer";
+import { actionMenuBundle } from "@credo-ts/drizzle-storage/action-menu";
+
+// const database = drizzle(
+// 	"postgresql://postgres:postgres@localhost:5432/holder-agent-id2"
+// );
+// SQL
+const database = drizzle(
+	"file:./holder_sqlite.db"
+);
+
+const bundles = [coreBundle, didcommBundle, actionMenuBundle, anoncredsBundle, questionAnswerBundle] as const;
 
 export const holder = new Agent({
 	config: {
-		label: "Holder Agent",
-		walletConfig: {
-			id: "holder-agent-id",
-			key: "holder-agent-key",
-		},
-		endpoints: ["http://localhost:6007/didcomm"],
 		logger: new ConsoleLogger(LogLevel.trace),
 	},
 	modules: {
 		// Storage Module
 		askar: new AskarModule({
-			ariesAskar,
+			askar,
+			store: {
+				id: "holder-agent-id-askar",
+				key: "holder-agent-key",
+				database: {
+					type: 'postgres',
+					config: {
+						host: 'localhost'
+					},
+					credentials: {
+						account: 'postgres',
+						password: 'postgres',
+					}
+				}
+			},
+			enableKms: true,
+			enableStorage: false
+		}),
+		
+		drizzleStorage: new DrizzleStorageModule({
+			database,
+			bundles: [...bundles],
+		}),
+		didcomm: new DidCommModule({
+			connections: {
+				autoAcceptConnections: true
+			},
+			endpoints: ["http://localhost:6007/didcomm"],
 		}),
 
 		// Connections module is enabled by default, but we can
 		// override the default configuration
-		connections: new ConnectionsModule({
-			autoAcceptConnections: true,
-		}),
+
 	},
 	dependencies: agentDependencies,
 });
 
-holder.registerInboundTransport(
-	new HttpInboundTransport({
+holder.didcomm.registerInboundTransport(
+	new DidCommHttpInboundTransport({
 		port: 6007,
 		path: "/didcomm",
 	}),
 );
-holder.registerOutboundTransport(new HttpOutboundTransport());
-holder.registerOutboundTransport(new WsOutboundTransport());
+holder.didcomm.registerOutboundTransport(new DidCommHttpOutboundTransport());
+holder.didcomm.registerOutboundTransport(new DidCommWsOutboundTransport());
--- examples/connection-reuse/index.ts
+++ examples/connection-reuse/index.ts
@@ -7,17 +7,18 @@ async function app() {
 	issuer.config.logger.info("Agents initialized!");
 
 	// Create invitation
-	const oobRecord = await issuer.oob.createInvitation();
+	const oobRecord = await issuer.didcomm.oob.createInvitation();
 
 	// We have to save the invitationDid to reuse the connection later
 	// and we have to use invitationDid again while creating the invitation
 	const invitationDid = oobRecord.outOfBandInvitation.invitationDids[0];
 
 	// We can reuse the connection if we want to by passing the `reuseConnection` flag to the receiveInvitation method
-	const rec = await holder.oob.receiveInvitation(
+	const rec = await holder.didcomm.oob.receiveInvitation(
 		oobRecord.outOfBandInvitation,
 		{
 			reuseConnection: true,
+			label: 'holder',
 		},
 	);
 
@@ -25,17 +26,18 @@ async function app() {
 		throw new Error("Connection record not found");
 	}
 
-	await holder.connections.returnWhenIsConnected(rec.connectionRecord?.id);
+	await holder.didcomm.connections.returnWhenIsConnected(rec.connectionRecord?.id);
 
 	// We pass the invitationDid here to create a new connection invitation
-	const oobRecord1 = await issuer.oob.createInvitation({
+	const oobRecord1 = await issuer.didcomm.oob.createInvitation({
 		invitationDid,
 	});
 
-	const oobRec = await holder.oob.receiveInvitation(
+	const oobRec = await holder.didcomm.oob.receiveInvitation(
 		oobRecord1.outOfBandInvitation,
 		{
 			reuseConnection: true,
+			label: 'issuer'
 		},
 	);
 
@@ -43,10 +45,38 @@ async function app() {
 		throw new Error("Connection record not found");
 	}
 
-	await holder.connections.returnWhenIsConnected(oobRec.connectionRecord?.id);
+	await holder.didcomm.connections.returnWhenIsConnected(oobRec.connectionRecord?.id);
 
-	const issuerConnections = await issuer.connections.getAll();
-	const holderConnections = await holder.connections.getAll();
+	const issuerConnections = await issuer.didcomm.connections.getAll();
+	const holderConnections = await holder.didcomm.connections.getAll();
+	// Postgres
+	// const holderConnectionsFilt = await holder.didcomm.connections.findAllByQuery({
+	// 	// $or: [
+	// 	// 	{ threadId: 'f3fd1417-5e58-4777-b23f-3fff5bebb038' },
+	// 	// 	{ threadId: 'd33f99a9-8436-4932-99b8-3b7058b55ed0' }
+	// 	// ]
+	// }, {
+	// 	cursor: {
+	// 		id: '6b6df230-ad3d-4459-9e00-6e7d98aa6f78',
+	// 		updatedAt: new Date('2026-01-12T10:54:35.996Z')
+	// 	}
+	// }) ;
+
+	// SQL
+	const holderConnectionsFilt = await holder.didcomm.connections.findAllByQuery({
+		// $or: [
+		// 	{ threadId: 'f3fd1417-5e58-4777-b23f-3fff5bebb038' },
+		// 	{ threadId: 'd33f99a9-8436-4932-99b8-3b7058b55ed0' }
+		// ]
+	}, {
+		// cursor: {
+		// 	id: 'a676e7f3-b8ce-4b3c-8493-a974fd02fdaf',
+		// 	// updatedAt: new Date('2026-01-13T06:43:45.815Z')
+		// },
+		offset: 1,
+		limit: 4
+	});
+
 
 	issuer.config.logger.info(
 		`Issuer Connections count: ${issuerConnections.length}`,
@@ -54,6 +84,9 @@ async function app() {
 
 	holder.config.logger.info(
 		`Holder Connections count: ${holderConnections.length}`,
+	);
+	holder.config.logger.info(
+		`Holder Connections: ${JSON.stringify(holderConnectionsFilt, null, 2)}`,
 	);
 }
 
